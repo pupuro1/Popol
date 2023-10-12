5d2246015dfb9b1a799e25884a5f0abf667e514a
BEOMTAEK <qmfpdlqm633@gmail.com>
2023-10-12 16:40:44 +0900

    css 수정/추가 박승균

--- fooding/src/routes/Login.js
+++ fooding/src/routes/Login.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import axios from 'axios';
-import { setCookie } from '../cookie';
-import { useNavigate } from 'react-router-dom';
-import '../scss/login.scss'
+import '../scss/login.scss';
+
 
 const Login = () => {
-  const navigate = useNavigate();
-  const onLogin = async (e) => {
+  const onLogin = (e) => {
     e.preventDefault()
     const id = e.target.user_id.value;
     const pwd = e.target.user_pwd.value;
-    // setCookie('login',id,);
-    await axios.post(`/login`,{id,pwd})
+    axios.post('/login',{id,pwd})
     .then(()=>{
-      setCookie('login',id);
-      navigate('/');
-      console.log('로그인 쿠키저장 완료');
+      
     })
     .catch((error)=>{
-      // console.error(error);
+      console.error(error);
       console.log('전송오류입니다.');
     })
   }
@@ -26,11 +21,11 @@ const Login = () => {
     <>
       <form onSubmit={onLogin} class="Loginid">
         <h2>로그인</h2>
-        <input id='user_id' type="text" placeholder='아이디'/>
-        <input id='user_pwd' type="password" placeholder='비밀번호'/>
+        <input type="text" placeholder='아이디'/>
+        <input type="password" placeholder='비밀번호'/>
         <div>
-          <label>
-        <input type="checkbox" ></input>아이디 저장
+          <label id="che">
+        <input type="checkbox"></input>아이디 저장
           </label>
             <a href=''><li>아이디 찾기</li></a>
             <a href=''><li>비밀번호 찾기</li></a>
